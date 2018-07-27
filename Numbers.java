369caf86d66cc12bb223e832e5faf8aeb0a544e5
ramesh4456 <41731698+ramesh4456@users.noreply.github.com>
2018-07-27 15:36:21 +0530

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+impoert java.util.Scanner;
+public class Numbers
+{
+public static void main(String[] args)
+int d=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<n;i++)
+		{
+		d=d+i;
+		}
+		System.out.println(d);
+}
