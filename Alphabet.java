59ea497ecb5152be97b093033bf9402529547b59
ramesh4456 <41731698+ramesh4456@users.noreply.github.com>
2018-07-27 11:46:18 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -4,8 +4,8 @@ public class Alphabet
 {
 public static void main(String[] args)
 {
-Scanner sc=new Scanner(System.in)
-int n=next().charAt(0);
+Scanner sc=new Scanner(System.in);
+int n=sc.next().charAt(0);
 if((n>='a'&&n<='z')||(n>='A'&&n<='Z'))
 System.out.println("Alphabet");
 else
