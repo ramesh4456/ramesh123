88783d2819f24407ba3eb60258e8722b4a664623
ramesh4456 <41731698+ramesh4456@users.noreply.github.com>
2018-07-27 11:58:20 +0530

    Create Biggest.java

---
+++ Biggest.java
@@ -0,0 +1,25 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc =new Scanner(System.in); 
+	
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		if(n>n1)
+		System.out.println("n");
+		else if(n>n2)
+		System.out.println("n1");
+		else 
+		System.out.println("n2");
+	}
+} 
