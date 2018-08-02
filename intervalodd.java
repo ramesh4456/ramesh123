739ac41a41b2bb9d9112f63aee899b8564011be2
ramesh4456 <41731698+ramesh4456@users.noreply.github.com>
2018-08-02 13:45:00 +0530

    Create intervalodd.java

---
+++ intervalodd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class intervalodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int m=s.nextInt();
+		int n=s.nextInt();
+			while(++m<n)
+			{
+				if(m%2!=0)
+				{
+					System.out.print(m+" ");
+				}
+				
+			}
+		}
+ 
+}
